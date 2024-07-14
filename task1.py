9092f8b293012628acaf93b1d3f31b28863bba57
lamr64 <sky64rus@yandex.ru>
2024-07-14 23:30:19 +0400

    The tasks completed.

---
+++ task1.py
@@ -0,0 +1,8 @@
+print('1st program')
+print(9**0.5*5)
+print('2nd program')
+print(9.99>9.98 and 1000!=1000.1)
+print('3rd program')
+print((1234%1000//10)+(5678%1000//10))
+print ('4th program') #13.42==42.13
+print((13.42 == 13 and 42) == (42.13 == 42 and 13))
