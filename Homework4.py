a2b9dea6ad3c80f3aefb3f44a3e70a4036734161
lamr64 <sky64rus@yandex.ru>
2024-07-20 14:03:34 +0400

    Homework 4 corrected

--- Homework4.py
+++ Homework4.py
@@ -1,5 +1,5 @@
-my_string = 'Привет дорогой друг! Как тебя зовут?'
-print(len(input(my_string)))
+my_string = input('Привет дорогой друг! Как тебя зовут? ')
+print(len(my_string))
 print(my_string[-1]) # Выведите последний символ строки my_string.
 print(my_string[0]) # Выведите первый символ строки my_string.
 print(my_string.replace(' ', '')) # Измените строку my_string, удалив все пробелы.
