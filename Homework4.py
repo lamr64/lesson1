ad248eac6a57fa53227b05c3fb0bbd394fed6e80
lamr64 <sky64rus@yandex.ru>
2024-07-20 08:01:19 +0400

    Homework 4 corrected

--- Homework4.py
+++ Homework4.py
@@ -1,9 +1,10 @@
 my_string = 'Привет дорогой друг! Как тебя зовут?'
-print(len(input(my_string[-1]))) # Выведите последний символ строки my_string.
-# print(len(input(my_string[0]))) # Выведите первый символ строки my_string.
-# print(len(input(my_string.replace(' ', '')))) # Измените строку my_string, удалив все пробелы.
-# print(len(input(my_string.lower()))) # Выведите строку my_string в нижнем регистре.
-# print(len(input(my_string.upper()))) # Выведите строку my_string в верхнем регистре.
+print(len(input(my_string)))
+print(my_string[-1]) # Выведите последний символ строки my_string.
+print(my_string[0]) # Выведите первый символ строки my_string.
+print(my_string.replace(' ', '')) # Измените строку my_string, удалив все пробелы.
+print(my_string.lower()) # Выведите строку my_string в нижнем регистре.
+print(my_string.upper()) # Выведите строку my_string в верхнем регистре.
 
 
 
