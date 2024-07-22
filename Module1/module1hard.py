9884f4372e5d247304bd382453b5c369ca653abf
lamr64 <sky64rus@yandex.ru>
2024-07-22 22:55:15 +0400

    module1

---
+++ Module1/module1hard.py
@@ -0,0 +1,6 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+sorted(students)
+grades_medium= sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]), sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4])
+dnevnik = dict(zip(students, grades_medium))
+print(dnevnik)
