84852be4e4f59f4c3cbee178639a6e766471267d
lamr64 <sky64rus@yandex.ru>
2024-07-27 20:13:39 +0400

    module2.1

---
+++ Module2/Homework1.py
@@ -0,0 +1,10 @@
+print('Здравствуйте, сегодня мы проверим возможности команд: if,elif,else.')
+first = input('Введите первое число: ')
+second = input('Введите второе число: ')
+third = input('Введите третье число: ')
+if first == second and second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
