12d09185e0b057385af1216c7b434e0c2ad53c59
lamr64 <sky64rus@yandex.ru>
2024-10-02 22:12:03 +0400

    module2.4

--- Module2/module_2_hard.py
+++ Module2/module_2_hard.py
@@ -3,17 +3,9 @@ def password(n):
     for i in range(1, int(n)):
         for j in range(i + 1, int(n)):
             sum_pass = j + i
-            if sum_pass % int(n) == 0:
+            if int(n) % sum_pass == 0:
                 pass_.append(i)
                 pass_.append(j)
-                # print(f"({i}+{j})={n}")
     return pass_
-
 result = password(input("Введите число от 3 до 20, чтобы подобрать пароль: "))
 print(*result, sep="")
-
-
-
-
-
-
