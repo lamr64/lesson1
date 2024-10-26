af4b03659e9564d884790cc34bb8048c056307bc
lamr64 <sky64rus@yandex.ru>
2024-10-26 20:40:26 +0400

    done

--- Module3/Module3_5.py
+++ Module3/Module3_5.py
@@ -1,8 +1,13 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
+    if first == 0:
+        if len(str_number) > 1:
+            return get_multiplied_digits(int(number[1:]))
+        else:
+            return 1
     if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
+            return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return first
 
